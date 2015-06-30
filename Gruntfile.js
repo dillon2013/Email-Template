ea1d0b9c57dbefeda8b2bb85a82dacd5c5c35589
dillon2013 <dillonlee@live.co.uk>
2015-06-30 22:07:44 +0100

    ejs configuration

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,26 @@
+var helpers = require('./my-helpers');
+
 module.exports = function(grunt){
+
+	// load the tasks that you want to use after installing with loadNpmTasks
+	grunt.loadNpmTasks('grunt-ejs');
+
+	// configure grunt tasks
+	grunt.initConfig({
+
+		pkg : grunt.file.readJSON('package.json'),
+
+		ejs : {
+			all : {
+				options : grunt.util_merge(helpers,{}),
+				src :'index.ejs',
+				dest : 'dist/index.html'
+			}
+		}
+
+	});
+
+	//create commands for terminal 
+	grunt.registerTask('build',['ejs']);
 
 }
